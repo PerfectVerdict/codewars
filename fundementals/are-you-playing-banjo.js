ae8a8dd51315ff26d89f22f24bcb7ca3d168c645
Ethan <76500833@protonmail.com>
2025-02-05 19:29:45 -0500

    refactored banjo file

--- fundementals/are-you-playing-banjo.js
+++ fundementals/are-you-playing-banjo.js
@@ -1,11 +1,16 @@
+// function areYouPlayingBanjo(name) {
+//   // Implement me
+//   if (name.charAt(0) === "r") {
+//     return name + " plays banjo";
+//   }
+//   if (name.charAt(0) === "R") {
+//     return name + " plays banjo";
+//   } else return name + " does not play banjo";
+// }
+// ? TBR
 function areYouPlayingBanjo(name) {
-    // Implement me
-      if (name.charAt(0) === "r") {
-        return name + " plays banjo"
-      }
-      if (name.charAt(0) === "R") {
-          return name + " plays banjo"
-      }
-      else return name + " does not play banjo"
-  }
-// Can be improved
+    if (name[0].toLowerCase() === 'r') {
+        return `${name} plays the banjo`;
+    }
+    return `${name} does not play banjo`
+}
