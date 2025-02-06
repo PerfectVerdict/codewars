92a0c3cb65b40ef584cdd64db790db54fdb303fb
Ethan <76500833@protonmail.com>
2025-02-05 19:10:08 -0500

    can be improved.

---
+++ fundementals/are-you-playing-banjo.js
@@ -0,0 +1,11 @@
+function areYouPlayingBanjo(name) {
+    // Implement me
+      if (name.charAt(0) === "r") {
+        return name + " plays banjo"
+      }
+      if (name.charAt(0) === "R") {
+          return name + " plays banjo"
+      }
+      else return name + " does not play banjo"
+  }
+// Can be improved
